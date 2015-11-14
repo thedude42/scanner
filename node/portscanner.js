d8c223d87ab6e4312549929526952a78fe0ce3f0
johnny <sk8rdie42@gmail.com>
2015-11-14 11:49:29 -0800

    Open ports report service names

--- node/portscanner.js
+++ node/portscanner.js
@@ -3,7 +3,8 @@
 var child_process = require("child_process"),
     path = require("path"),
     dns = require("dns"),
-    EventEmitter = require("events");
+    readline = require("readline"),
+    fs = require("fs");
 
 if (process.argv.length < 3) {
     console.log("Please supply an address to scani\n\nusage:\n\t", path.basename(process.argv[1]), "<address>");
@@ -16,6 +17,7 @@ var SCANNER_MODULE = "childscanner.js",
     NUM_PORTS = 65535,
     ADDR = process.argv[2];
 
+// Module vars
 var children = [],
     results = {
         open:[],
@@ -23,10 +25,12 @@ var children = [],
         filtered:[],
         scanned:[]
     },
+    ServicesDBTcp = {},
     guard = jobGate(JOBS);
 
 console.log("Starting",JOBS,"child processes for scanning address",ADDR);
 
+// Perform lookup on addr input arg, then init the services DB and start scan
 dns.lookup(ADDR, function onDnsLookup(err, address, fam) {
     if (err) {
         console.log("Unable to resolve", ADDR);
@@ -34,9 +38,26 @@ dns.lookup(ADDR, function onDnsLookup(err, address, fam) {
     }
     else {
         console.log("need to regex this address for IP addr format");
-        beginScan();
+        initServicesObject(ServicesDBTcp, beginScan);
     }
 });
+
+function initServicesObject(obj, cb) {
+    var record_regex = /(\S+)\s+(\d+)\/tcp.+/,
+        filestream = fs.createReadStream("/etc/services"),
+        rl = readline.createInterface({input:filestream}),
+        services = {};
+    rl.on("line", function(line) {
+        var m = record_regex.exec(line);
+        if (m) {
+            obj[m[2]] = m[1];
+        }
+    });
+    filestream.on("close", cb);
+    filestream.on("error", function(e) {
+        console.log("could not open /etc/services : ",e);
+    });
+}
 
 function countResults() {
     return results.open.length+
@@ -74,9 +95,8 @@ function initChild(num) {
         }
     });
     child.on("message", function parentMsgHandler(msg) {
-        
         if (msg.state === "open") {
-            results.open.push(msg.port);
+            results.open.push(msg.port+": "+ServicesDBTcp[msg.port]);
         }
         else if (msg.state === "closed") {
             results.closed.push(msg.port);
@@ -86,6 +106,7 @@ function initChild(num) {
         }
         results.scanned[msg.port] = true;
         child.assigned[msg.port] = "scanned";
+        // All work complete case
         if (countResults() == NUM_PORTS) {
             console.log("DISCONNECTING",children.length,"CHILDREN");
             children.forEach(function(c) {
@@ -93,13 +114,14 @@ function initChild(num) {
             });
         }
     });
+    // default case
     if (arguments.length < 2) {
         console.log("Sending port messages to child",child.num);
         for (var port = child.num+1; port <= NUM_PORTS; port += JOBS) {
             child.send({port:port,addr:ADDR});
             child.assigned[port] = "waiting";
         }
-    }
+    } // restarting failed child case with "waiting" ports array argument
     else {
         console.log("re-initializing child",child.num); 
         arguments[1].forEach(function(port) {
